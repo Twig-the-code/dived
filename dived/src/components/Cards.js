ad7c8baf36d10d07704f70fd3cb466c4242ffc59
Aki Salmi <aki.salmi@iki.fi>
2019-08-29 17:20:01 +0300

    can filter cards

--- dived/src/components/Cards.js
+++ dived/src/components/Cards.js
@@ -1,13 +1,13 @@
-import {I18n} from 'aws-amplify';
+import { I18n } from 'aws-amplify';
 import React from 'react';
 import PropTypes from 'prop-types';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCheckSquare,
   faListAlt,
   faEye,
 } from '@fortawesome/free-regular-svg-icons';
-import Card, {CardActions} from './Card';
+import Card, { CardActions } from './Card';
 import {} from './Cards.css';
 
 const isFinished = (finishedCards, card) => {
@@ -17,35 +17,86 @@ const getFinished = (finishedCards, card) => {
   return isFinished(finishedCards, card) ? 'finished' : 'unfinished';
 };
 
-const Filter = () => {
+const Filter = props => {
+  const { filters, currentFilter, onClick } = props;
   return (
     <div className="filter-container">
-      <button type="button" className="filter filter--active">
-        <FontAwesomeIcon icon={faEye}/>
+      <button
+        type="button"
+        className={`filter ${
+          currentFilter === filters.all ? 'filter--active' : ''
+        }`}
+        onClick={() => onClick(filters.all)}
+      >
+        <FontAwesomeIcon icon={faEye} />
         <span className="filter__all">{I18n.get('filter all')}</span>
       </button>
-      <button type="button" className="filter">
-        <FontAwesomeIcon icon={faListAlt}/>
+      <button
+        type="button"
+        className={`filter ${
+          currentFilter === filters.unfinished ? 'filter--active' : ''
+        }`}
+        onClick={() => onClick(filters.unfinished)}
+      >
+        <FontAwesomeIcon icon={faListAlt} />
         <span className="filter__unfinished">
           {I18n.get('filter unfinished')}
         </span>
       </button>
-      <button type="button" className="filter">
-        <FontAwesomeIcon icon={faCheckSquare}/>
+      <button
+        type="button"
+        className={`filter ${
+          currentFilter === filters.finished ? 'filter--active' : ''
+        }`}
+        onClick={() => onClick(filters.finished)}
+      >
+        <FontAwesomeIcon icon={faCheckSquare} />
         <span className="filter__finished">{I18n.get('filter finished')}</span>
       </button>
     </div>
   );
 };
-Filter.propTypes = {};
+Filter.propTypes = {
+  filters: PropTypes.shape({
+    all: PropTypes.string.isRequired,
+    unfinished: PropTypes.string.isRequired,
+    finished: PropTypes.string.isRequired,
+  }),
+  currentFilter: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+const FILTERS = {
+  all: 'all',
+  unfinished: 'unfinished',
+  finished: 'finished',
+};
+
+const FILTER_FUNCS = {
+  all: props => () => true,
+  unfinished: finishedCards => c => !isFinished(finishedCards, c),
+  finished: finishedCards => c => isFinished(finishedCards, c),
+};
 
 class Cards extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { filter: FILTERS.all };
+  }
+
+  setFilter(filter) {
+    this.setState({ filter });
+  }
+
   createAllCards(cards) {
-    return cards.map(this.createCard());
+    const { filter } = this.state;
+    const { finishedCards } = this.props;
+    const filterFunc = FILTER_FUNCS[filter]
+    return cards.filter(filterFunc(finishedCards)).map(this.createCard());
   }
 
   createCard() {
-    const {finishedCards, actions} = this.props;
+    const { finishedCards, actions } = this.props;
     return card => {
       const action = isFinished(finishedCards, card)
         ? actions.markCardAsOpen
@@ -62,10 +113,15 @@ class Cards extends React.Component {
   }
 
   render() {
-    const {cards} = this.props;
+    const { filter } = this.state;
+    const { cards } = this.props;
     return (
       <div>
-        <Filter/>
+        <Filter
+          filters={FILTERS}
+          currentFilter={filter}
+          onClick={e => this.setFilter(e)}
+        />
         <div className="card-container">{this.createAllCards(cards)}</div>
       </div>
     );
